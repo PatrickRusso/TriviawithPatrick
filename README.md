5771daa5058f07ea60f92046efd903de8780f448
PatrickRusso <113122527+PatrickRusso@users.noreply.github.com>
2023-02-03 13:22:13 -0500

    psuedo code mostly completed

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # TriviawithPatrick
-Trivia game
+Music Producer themed multiple choice quiz.
+
+GIF of people in the studio making a beat
+
+"A music producer quiz"
+
+Key Features
+
+-The game will be 4 multiple choice questions about different music producers in the rap genre.
+-The user can select between a day time studio or a night time studio with LED lights.
+
+MVP
+
+-Functional quiz with one question taking the user to the next, until met with a final screen showing their quiz score.
+-Replay button allowing them to take the quiz again.
+
+
+Post MVP
+
+-Light mode is a daytime studio 
+-Dark mode is a night time studio
+-Animations to make the quiz look cooler
+-Music playing in the background
+
+
+Time management 
+
+day 1 - Have a nice looking ReadMe file on github
+day 2 - create a non functional loading screen
+day 3- work on getting buttons in place and taking the user to the next question
+day 4- game logic tracking score and other functionalities.
+day 5- make the quiz look nice 
+
+
+
---
+++ index.js
@@ -0,0 +1,66 @@
+//TRIVIA GAME OUTLINE
+//music producer quiz
+
+//In my trivia game, the user will load up their browser and be met with an alert asking them to type in their name.
+
+//after a name is typed in the bar, the user will be directed to the main menu of the game, with their name being displayed somehow at the top
+
+//if i can play an instrumental while the user is taking the quiz I will but I am not sure how that works.
+
+//Here the user is met with a Test your knowledge button(Play), a change theme button(light mode or dark mode), 
+//need more ideas above but these are the buttons I am sure about.
+
+//after the user clicks dark mode (because you are insane if you don't, the screen will turn a cool black and purple gradient)
+//Dark mode could either be space theme or futuristic theme
+
+//If the user is insane and wants to go light mode it will be something like a 64bit pen and paper styled game with a non default white background and black text.(maybe the background will be a school desk with paper and a pencil, with the game being played on paper.)
+
+//After the user chooses their respective theme, they will be directed to the quiz.
+
+//the quiz will most likely be multiple choice but it could have and opionated short answer question for the last question so the user automatically gets that one correct.
+//maybe the last question will be 'How did you feel about project1? with a congratulations screen after submitted.
+
+//START OF THE QUIZ
+
+//First question
+
+//Which young producer was responsible for Juice Wrld's hit song, All Girls are the Same?
+
+//a. 
+//b.
+//c.
+//d.
+
+//Second question
+
+//With Chopsquad DJ being a major influence in modern rap music, where was he born and which rapper helped him get his foot in the game?
+
+//Third question
+
+//
+
+//a.
+//b.
+//c.
+//d.
+
+//fourth question
+
+//
+
+//a.
+//b.
+//c.
+//d.
+
+//Fifth and final question
+
+//
+
+//a.
+//b.
+//c.
+//d.
+
+//some sort of congradulations screen with a percentage showing how many they got correct. Maybe the last question will be correct no matter what so nobody gets a 0 on my quiz.
+//here the user will be met with a replay button.
