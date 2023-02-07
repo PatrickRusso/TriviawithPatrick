9b00dceaf23ba17a83206bc6f92c50ac3bb1fb45
PatrickRusso <113122527+PatrickRusso@users.noreply.github.com>
2023-02-07 17:50:56 -0500

    Progress on js

--- index.js
+++ index.js
@@ -6,13 +6,35 @@ let questions = {
             'Young Chop',
             'Chopsquad DJ',
             'Hitboy'
-        ]
+        ],
+        correctIndex: 2
     },
 }
+
+let questionIndex = 0
+let currentQuestion = questions[questionIndex]
+
 let submitButton = document.querySelector('#question-submit')
 let form = document.querySelector
-submitButton.addEventListener('click', () => {
 
+submitButton.addEventListener('click', () => {
+    let options = document.querySelectorAll ('option')
+    let choseCorrect = false
+    let i = 0
+
+    options.forEach(element => {
+        if(element.checked = true && currentQuestion.correctIndex == [i]) {
+            //This is where we figure out if user is correct
+            choseCorrect = true
+        }
+        i++
+    });
+
+    if(!choseCorrect) {
+        //score doesnt move
+    }
+    questionIndex += 1
+    currentQuestion = questions[questionIndex]
 })
 
 
