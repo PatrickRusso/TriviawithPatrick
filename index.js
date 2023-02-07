3d939adb98b1db021c0a015b82e1959ecfcd621e
PatrickRusso <113122527+PatrickRusso@users.noreply.github.com>
2023-02-07 17:21:30 -0500

    working on changing  question and answer format

---
+++ index.js
@@ -0,0 +1,44 @@
+let questions = {
+    questionOne: {
+        question: 'This producer made the hit song Wishing Well:',
+        answers: [
+            'Nick Mira',
+            'Young Chop',
+            'Chopsquad DJ',
+            'Hitboy'
+        ]
+    },
+}
+let submitButton = document.querySelector('#question-submit')
+let form = document.querySelector
+submitButton.addEventListener('click', () => {
+
+})
+
+
+
+
+
+
+
+
+
+
+//Need a play button and a next button
+
+//const playButton
+
+//const nextQuestion
+
+//need an array of questions and answers with only one answer = true and the rest = false
+
+//let questions = []
+
+//need a function to start the game on click
+
+//need a function to check the array for the correct answer on click
+
+//need a function to display the next question on click
+
+
+
